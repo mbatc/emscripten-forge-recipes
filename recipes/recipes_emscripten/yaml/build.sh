ca5dc4846f13469e01c36b921c368a42598e4cdd
Michael Batchelor <mickbatch98@gmail.com>
2023-08-27 22:12:13 +1000

    Add libyaml and changed spdlog version

---
+++ recipes/recipes_emscripten/yaml/build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Configure step
+emconfigure ./configure --prefix=${PREFIX} \
+            --enable-static     \
+            --disable-shared
+
+make -j${CPU_COUNT} ${VERBOSE_AT}
+make install
